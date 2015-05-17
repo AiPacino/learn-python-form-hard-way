2b42021c2fb4c8792433805568200108dc3af46d
AiPacino <540717461@qq.com>
2015-05-17 09:36:00 +0800

    习题5

---
+++ 习题3/ex3.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*- 
+print("I will now count my chickens")
+
+#这行是为了用计算25 + 30 /6 的值
+print("Hens", 25 + 30 / 6)
+print("Hens", 25.0 + 30.0 / 6.0)
+
+#这行是为了求100 - 25 * 3 % 4
+print("Roosters", 100 - 25 * 3 % 4)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+
+print("Now I will count the eggs")
+
+#这一行是为了求3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+#这一行是为了求3+2 < 5 -7
+print("Is it true that 3 + 2 < 5 - 7")
+
+#这一行是为了求3 + 2 < 5 - 7
+print( 3 + 2 < 5 - 7)
+
+#这一行是为了求3 + 2
+print("What is 3 + 2?", 3 + 2)
+
+#这一行是为了求5 - 7
+print("What is 5 - 7?", 5 - 7)
+
+
+print("Oh, that`s why it`s False.")
+
+print("How about some more")
+
+#这一行是为了求真值
+print("Is it greater?", 5 > -2)
+
+#这一行是为了求真值
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ 习题5/ex5.py
@@ -0,0 +1,16 @@
+my_name = "Zed A. Shaw"
+my_age = 35
+my_height = 74
+my_weight = 180
+my_eyes = 'Blue'
+my_teech = 'White'
+my_hair = 'Brown'
+
+print("Let`s talk about", my_name ,".")
+print("He`s ",my_height ," inches tall.")
+print("He`s ",my_weight ,"pounds heavy")
+print("Actually that`s not to heavy.")
+print("He`s got ",my_eyes ,"eyes and ",my_hair ," hair.")
+print("His teeth are usually ",my_teech ,"depending on the coffee.")
+
+print("If I add ",my_age ,my_height ,my_weight ," I get ",my_age + my_height + my_weight)
